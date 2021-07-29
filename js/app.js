22ca31c9a4c899637558d355590e7e9e754263b9
Agustin Leguizamon <agustin.leg7@gmail.com>
2021-07-29 17:13:42 -0300

    Clase 08 DOM

--- js/app.js
+++ js/app.js
@@ -1,97 +1,107 @@
 const admin = "agustin";
 let productosContenedor = [];
+let productosCarrito = document.getElementById("productosCarrito");
 
-function cuentaRegistrada() {
-  let usuario = prompt("Tienes una cuenta registrada? \n si - no");
-  switch (usuario) {
-    case "si":
-      cuentaAdmin();
-      break;
-    case "no":
-      alert("bienvenido a @CaseShop");
-      break;
 
-    default:
-      cuentaRegistrada();
-      break;
-  }
-}
-
-class Producto{
+class Producto {
   constructor(id, nombre, precio) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
-    this.cantidad = 0;
-  }
-  sumarIVA(){
-      this.precio *= 1.21;
-  }
-  reponer(){
-    this.cantidad += parseInt(prompt(`Unidades de ${this.nombre}`));
-    if(isNaN(this.cantidad)){
-        while(isNaN(this.cantidad)){
-            this.cantidad = prompt('Cantidad Incorrecta');
-        }
-    }
-  }
-  vender(){
-    let confirmarVenta = prompt(`Quieres vender 15 unidades del producto: ${this.nombre}? \n si - no`);
-    if(confirmarVenta.toLowerCase() === 'si'){
-        if(this.cantidad >= 15){
-            this.cantidad -= 15;
-            alert(`Vendido exitosamente a $${this.precio} c/u \n unidades restantes ${this.cantidad}`)
-        }else{
-            alert(`Cantidad ${this.cantidad} insuficiente`)
-        }
-    }
+    this.cantidad = 1;
   }
 }
 
-function cuentaAdmin() {
-  let cuentaUsuario = prompt("Nombre de usuario:");
-  if (cuentaUsuario.toLowerCase() === admin) {   
-    while(prompt('Quiere agregar productos \n ESC para salir') !== 'ESC'){
-      let idProducto = Number(prompt('id'));
-      let existe = productosContenedor.some(producto => producto.id === idProducto )
-      if(!existe){
-        productosContenedor.push(new Producto(idProducto,prompt('nombre'),prompt('precio')));
-      }else{
-        alert(`ERROR \n YA EXISTE UN PRODUCTO CON EL ID: ${idProducto}`)
-      }
+function comprarProductos() {
+  while ( prompt( "Quiere agregar productos al carrito? \n ESC para salir").toLowerCase() !== "esc") {
+    let idProducto = new Date().getTime();
+    let nombreProducto = prompt("nombre").toLowerCase();
+    let existe = productosContenedor.some(
+      (producto) => producto.nombre === nombreProducto
+    );
+    if (!existe) {
+      productosContenedor.push(
+        new Producto(idProducto, nombreProducto, prompt("precio"))
+      );
+    } else {
+      alert(`ERROR \n YA EXISTE UN PRODUCTO CON ESE NOMBRE: ${nombreProducto}`);
     }
-    let alertaProductos = 'Productos agregados: \n'
-   
+  }
 
-    // ---------- DESAFIO EXTRA ----------------
-    let avisoOrdenar = prompt('Queres ver los productos ordenados alfabeticamente? \n SI - NO')
-    if(avisoOrdenar.toLowerCase() === 'si'){
-      let productosOrdenados = productosContenedor.sort((a,b) => {
+  if (productosContenedor.length >= 1) {
+    // Ordenar alfabeticamente
+    let avisoOrdenar = prompt(
+      "Queres ver los productos ordenados alfabeticamente? \n SI - NO"
+    );
+    if (avisoOrdenar.toLowerCase() === "si") {
+      productosContenedor.sort((a, b) => {
         const nombreA = a.nombre.toLowerCase();
         const nombreB = b.nombre.toLowerCase();
-        if(nombreA < nombreB){
+        if (nombreA < nombreB) {
           return -1;
         }
-        if(nombreA > nombreA){
+        if (nombreA > nombreA) {
           return 1;
         }
         return 0;
-      })
-      for(producto of productosOrdenados){
-        alertaProductos += `${producto.nombre} precio: $${producto.precio} ID:${producto.id} \n`
-      }
-      alert(alertaProductos)
-    }else{
-      for(producto of productosContenedor){
-        alertaProductos += `Nº ${producto.id} nombre: ${producto.nombre} precio: $${producto.precio} \n`
-      }
-      alert(alertaProductos);
+      });
     }
-    // -----------------------------------------------
-    
-  } else {
-    alert(`Bienvenido ${cuentaUsuario}`);
+  }
+  agregarCarrito(productosContenedor);
+}
+
+
+// Agregar los productos al carrito
+function agregarCarrito(productos) {
+  for (producto of productos) {
+    let producto = document.createElement("div");
+    producto.classList.add("producto");
+    producto.innerHTML = `
+          <img src="images/funda-iphone-amarillo.png" class="d-block w-100" alt="Hombre sosteniendo su celular con una funda negra">
+          <div class="producto__info">
+              <p class="producto__titulo">${this.producto.nombre}</p>
+              <p class="producto__precio">$${this.producto.precio}</p>
+              <div id="btnCantidad">
+                  <button type="button" class="restarCantidad"> - </button>
+                  <p class="cantidad">${this.producto.cantidad}</p>
+                  <button type="button" class="sumarCantidad"> + </button>
+              </div>
+          </div>
+          <a href="#" class="eliminar-btn"><i class="far fa-trash-alt"></i></a>
+    `;
+    productosCarrito.appendChild(producto);
+
+  
+    estadoCarrito();
   }
 }
 
-cuentaRegistrada();
+// Verificar si el carrito esta vacio - actualizar estado y cantidad
+function estadoCarrito(){
+  const cantidadCarrito = document.getElementsByClassName("count");
+  const cartProductos = document.querySelector('.cart__productos')
+  
+  // Actualizar cantidad del icono Carrito
+  cantidadCarrito[0].textContent = productosContenedor.length;
+
+  const carritoInfo = document.querySelector('#estadoCarrito');
+  
+  
+  if(productosCarrito.children.length < 1){
+    carritoInfo.innerHTML= `<span>El carrito de compras está vacío</span>`
+    cartProductos.appendChild(carritoInfo)
+  }else{
+    carritoInfo.innerHTML=`
+          <div class="cart__total" id="cartTotal">
+            <p class="total">Total: <span class="precio">$0</span></p>
+          </div>
+          <div class="btn-finalizar-compra">
+              <button class="btn btn-primary"><a href="#">Finalizar Compra</a></button>
+          </div>
+    `
+    cartProductos.appendChild(carritoInfo)
+  }
+}
+
+estadoCarrito();
+comprarProductos();
